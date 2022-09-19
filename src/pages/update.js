3007ec1fb20b9a16468a41f488bbc3d62347827e
Filip John <filipjaj@gmail.com>
2022-09-19 21:15:24 +0200

    is this working?

--- src/pages/update.js
+++ src/pages/update.js
@@ -4,10 +4,10 @@ import PlaceForm from '../../components/PlaceForm';
 export default function Update() {
  
   return (
-    <div className={styles.container}>
+   
 
       <PlaceForm />
 
-      </div>
+    
   )
 }
