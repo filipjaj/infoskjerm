aa86bb32838cfcbae7c8d5e71c143148656fb93d
Filip John <filipjaj@gmail.com>
2022-09-19 21:08:04 +0200

    uploaded icons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/globals.scss"
-
+import { useState } from "react"
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import { useCollection } from 'react-firebase-hooks/firestore';
-
+import {useQuery} from '@tanstack/react-query'
 import { doc, setDoc } from "firebase/firestore"; 
 import { db } from '../../lib/firebase';
 import PlaceForm from '../../components/PlaceForm';
+import axios from 'axios';
+const getWeather = async (lat, lon) => {
+  const data = await axios.get(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lon}`);
+
+  return data.data;
+}
 
 export default function Home() {
- 
+  const {data, isLoading} = useQuery(['weather'], () => getWeather(59.944991, 10.724816));
+ if(isLoading) return <div>Loading...</div>
+ console.log(data);
   return (
     <div className={styles.container}>
 
      <h1>Infoskjerm</h1>
+     <div className={styles.weatherCard}>
+     
+      <Image src={`/weathericon/svg/${data.properties.timeseries[0].data.next_1_hours.summary.symbol_code}.svg`} alt="Weather" width={200} height={200} />
+      <p>{data.properties.timeseries[0].data.instant.details.air_temperature} °C</p>
+      </div>
 
       </div>
   )
